ea2827c8c5c42cd6ea0158989acd431db80e6237
Thieu Viet Hoang <thieuhoangent@gmail.com>
2025-03-22 12:52:56 +0700

    update xoa mongodb

--- networks/database.py
+++ networks/database.py
@@ -1,34 +1,17 @@
-from bson import ObjectId
-from pymongo import MongoClient
+import socket
 
-# Thay thế các giá trị này bằng thông tin kết nối thực tế từ MongoDB Cloud của bạn
-username = 'admin_gamebansung'
-password = 'admin134'
-cluster_url = 'cluster0.ba6xpv6.mongodb.net'
-database_name = 'FPS_Game'
-
-# Tạo URL kết nối MongoDB
-uri = f'mongodb+srv://{username}:{password}@{cluster_url}/{database_name}?retryWrites=true&w=majority'
-
-# Kết nối tới MongoDB
-client = MongoClient(uri)
-
-# Chọn cơ sở dữ liệu
-db = client[database_name]
-
-# Lấy một collection từ cơ sở dữ liệu
-collection = db['server']
-
-def getIpServer():    
-    ipServer = collection.find_one({'_id': ObjectId('67d050474fb03c8bcd630f6b')})
-    print('ipServer', ipServer)
-    print(ipServer['ip'])
-    return ipServer['ip']
+def getIpServer():
+    # Lấy địa chỉ IPv4 của máy tính hiện tại
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    print('Địa chỉ IPv4 hiện tại:', ip_address)
+    return ip_address
 
 def setIpServer(ipServer):
-    collection.update_one({'_id': ObjectId('67d050474fb03c8bcd630f6b')}, {'$set': {'ip': str(ipServer)}})
+    # Hàm này không còn cần thiết khi không sử dụng MongoDB
+    print(f'Đã nhận địa chỉ IP mới: {ipServer}')
+    # Nếu cần lưu trữ địa chỉ IP vào file hoặc nơi khác, bạn có thể thêm logic tại đây
 
-
-
-# setIpServer('192.168.0.1')
-# getIpServer()
+# Ví dụ sử dụng
+# setIpServer('192.168.0.1')  # Không cần thiết nếu không lưu trữ
+getIpServer()
