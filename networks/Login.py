f72cdcbe923735e9421a8f4f932e08bffdbf6572
thieuhoang2002 <thieuhoangent@gmail.com>
2025-04-19 16:28:44 +0700

    update

--- networks/Login.py
+++ networks/Login.py
@@ -1,207 +1,161 @@
-from tkinter import Tk, Label, Entry, Button, Canvas, font, messagebox
-from PIL import ImageTk, Image
-import webbrowser
-
-from ursina import Func
-
+from cProfile import label
+from tkinter import Tk,Canvas,messagebox
+from PIL import Image,ImageTk
+from tkinter import Label,Entry,Text, Button,PhotoImage,Checkbutton,IntVar
 
 class LoginForm:
     def __init__(self, callback):
-        self.callback = callback
+        self.callback =callback
+        self.win = Tk()
+        self.win.title("Ursina")
+        # Kích thước cửa sổ
+        self.win.update_idletasks()  # Cập nhật để lấy kích thước chính xác
 
-        self.win = Tk()  # khoi tao bien "win" la cua so Tk()
-        self.win.title('Blaze Battlegrounds')
+        win_width = 1280
+        win_height = 720
 
-        # Lấy kích thước của màn hình
+        # Lấy kích thước màn hình
         screen_width = self.win.winfo_screenwidth()
         screen_height = self.win.winfo_screenheight()
 
-        # Kích thước của cửa sổ
-        win_width = 880 + 250
-        win_height = 520 + 100
+        # Tính toán vị trí để căn giữa
+        x = (screen_width - win_width) // 2
+        y = (screen_height - win_height) // 2
+        self.win.after(10, lambda: self.win.geometry(f"{win_width}x{win_height}+{x}+{y}"))
 
-        # Tính toán vị trí để cửa sổ xuất hiện ở giữa màn hình
-        x_position = (screen_width - win_width) // 2
-        y_position = (screen_height - win_height) // 2
+        # Đặt kích thước và vị trí cửa sổ
+        # Màu nền
+        self.win.configure(bg="#202227")
 
-        self.win.geometry(f'{win_width}x{win_height}+{x_position}+{y_position}')
+        canvas = Canvas(self.win, width=win_width, height=win_height, highlightthickness=0, bg="#202227")
+        canvas.place(x=0, y=0)
 
-        self.win.overrideredirect(True)
+        img = Image.open("asset/static/login_gui/2151138370.jpg")
+        img = img.resize((int(win_width * 0.7), win_height))
+        bg_photo = ImageTk.PhotoImage(img)
+        canvas.create_image(0, 0, anchor='nw', image=bg_photo)
+        canvas.image = bg_photo
 
-        self.win['bg'] = '#233657'
+        logo_x = int(win_width * 0.7) + 50
+        logo_y = 20  # Cùng với vị trí của tên label
+        img_logo = Image.open("asset/static/login_gui/flaticon.png")
+        img_logo = img_logo.resize((290,190))
+        bg_logo = ImageTk.PhotoImage(img_logo)
+        canvas.create_image(logo_x, logo_y, anchor='nw', image=bg_logo)
 
-        # Chèn ảnh
-        # Load hình ảnh từ đường dẫn
-        image_path = "asset/static/login_gui/background_33pt.png"
-        original_image = Image.open(image_path)
+        # Tọa độ bắt đầu bên phải ảnh
+        right_x = int(win_width * 0.7) + 50
+        label_y = 200
+        entry_y = label_y + 50  
 
-        # Điều chỉnh kích thước ảnh
-        new_size = (768, 417)
-        resized_image = original_image.resize(new_size)
+        # Label "Nhập tên người chơi"
+        name_label = Label(self.win, text="Welcome", fg="white", bg="#202227", font=("Arial", 22))
+        name_label.place(x=right_x + 70, y=label_y)
 
-        # Tạo một đối tượng PhotoImage từ hình ảnh
-        photo = ImageTk.PhotoImage(resized_image)
+        # Entry để nhập tên
+        self.name_entry = Entry(self.win, font=("Arial", 16), width=24, bg="#32353c", fg="#d7d7da")  # Màu chữ khi chưa nhập
+        self.name_entry.insert(0, "Nhập tên người chơi")  # Đặt placeholder ban đầu
+        self.name_entry.place(x=right_x, y=entry_y, height=40)  # hoặc dùng ipady với .pack() / .grid()
 
-        # Tạo một nhãn (label) để hiển thị hình ảnh và điều chỉnh vị trí
-        label = Canvas(self.win, width=win_width, height=win_height, bg='#233657', highlightthickness=0)
-        label.create_image(0, 0, anchor='nw', image=photo)
-        label.image = photo  # Giữ một tham chiếu đến hình ảnh để tránh bị hủy
-        label.place(x=50, y=50)  # Điều chỉnh vị trí của nhãn
+        #icon facebook
+        icon_facebook = Image.open("asset/static/login_gui/facebook.png")
+        icon_facebook = icon_facebook.resize((50,25))
+        facebook_photo = ImageTk.PhotoImage(icon_facebook)
 
-        # Khởi tạo biến chữ Fanpage
-        fanpageTxt = Label(self.win, text='Fanpage', font=('Times New Roman', 14), bg='#233657', fg='white')
-        fanpageTxt.place(x=(win_width - fanpageTxt.winfo_reqwidth()) // 2, y=20)
+        facebook_button = Button(self.win,image=facebook_photo,bg ="#202227",border=0, highlightthickness=0, relief="flat")
+        facebook_button.place(x=right_x, y=entry_y + 60)
 
-        # Gán sự kiện click cho chữ Fanpage
-        fanpageTxt.bind("<Button-1>", lambda event: self.open_fanpage())
+        #icon google
+        icon_google = Image.open("asset/static/login_gui/google.png")
+        icon_google = icon_google.resize((50,25))
+        google_photo = ImageTk.PhotoImage(icon_google)
 
-        # Gán sự kiện di chuột vào và ra khỏi widget
-        fanpageTxt.bind("<Enter>", self.change_cursor(self))
-        fanpageTxt.bind("<Leave>", self.revert_cursor(self))
+        google_button = Button(self.win,image=google_photo,bg= "#202227",border=0, highlightthickness=0, relief="flat")
+        google_button.place(x=right_x + 60,y=entry_y+ 60)
 
-        # Khởi tạo biến chữ Home
-        homeTxt = Label(self.win, text='Home', font=('Times New Roman', 14), bg='#233657', fg='white')
-        homeTxt.place(x=(win_width - fanpageTxt.winfo_reqwidth()) // 2 - 200, y=20)
+        #icon apple
+        icon_apple = Image.open("asset/static/login_gui/apple.png")
+        icon_apple = icon_apple.resize((50,25))
+        apple_photo = ImageTk.PhotoImage(icon_apple)
 
-        # Gán sự kiện click cho chữ Home
-        homeTxt.bind("<Button-1>", lambda event: self.open_home())
+        apple_button = Button(self.win,image = apple_photo, bg= "#202227",border=0, highlightthickness=0, relief="flat")
+        apple_button.place(x=right_x + 120,y=entry_y + 60)
 
-        # Gán sự kiện di chuột vào và ra khỏi widget
-        homeTxt.bind("<Enter>", self.change_cursor(self))
-        homeTxt.bind("<Leave>", self.revert_cursor(self))
+        #icon xbox 
+        icon_xbox = Image.open("asset/static/login_gui/xbox.png")
+        icon_xbox = icon_xbox.resize((50,25))
+        xbox_photo = ImageTk.PhotoImage(icon_xbox)
 
-        # Khởi tạo biến chữ Twitter
-        twitterTxt = Label(self.win, text='Twitter', font=('Times New Roman', 14), bg='#233657', fg='white')
-        twitterTxt.place(x=(win_width - fanpageTxt.winfo_reqwidth()) // 2 + 200, y=20)
+        xbox_button = Button(self.win,image=xbox_photo,bg= "#202227",border=0, highlightthickness=0, relief="flat")
+        xbox_button.place(x=right_x + 180, y= entry_y +60)
 
-        # Gán sự kiện click cho chữ Tw
-        twitterTxt.bind("<Button-1>", lambda event: self.open_twitter())
+        #icon sony
+        icon_sony = Image.open("asset/static/login_gui/playstation.png")
+        icon_sony = icon_sony.resize((50,25))
+        sony_photo = ImageTk.PhotoImage(icon_sony,)
 
-        # Gán sự kiện di chuột vào và ra khỏi widget
-        twitterTxt.bind("<Enter>", self.change_cursor(self))
-        twitterTxt.bind("<Leave>", self.revert_cursor(self))
+        sony_button = Button(self.win, image= sony_photo,bg= "#202227",border=0, highlightthickness=0, relief="flat")
+        sony_button.place(x=right_x + 240,y=entry_y + 60)
 
-        # Khởi tạo biến chữ Blaze Battlegrounds - Win or loss based on your skills
-        sloganTxt = Label(self.win, text='Blaze Battlegrounds - Win or loss based on your skills',
-                          font=('Times New Roman', 14),
-                          bg='#233657', fg='white')
-        sloganTxt.place(x=(win_width - sloganTxt.winfo_reqwidth()) // 2, y=470)
+        #checkbox
+        remember_var = IntVar()
+        remember_check = Checkbutton(
+            self.win,
+            text="Ghi nhớ đăng nhập",
+            variable=remember_var,
+            fg="white",
+            bg="#202227",
+            font=("Arial", 12),
+            activebackground="#202227",
+            activeforeground="white",
+            selectcolor="#202227",  # màu nền khi được tick
+            highlightthickness=0,
+            bd=0
+        )
+        remember_check.place(x=right_x, y=entry_y + 100)  # Điều chỉnh vị trí cho đẹp
 
-        # Tạo textbox
-        self.nameTb = Entry(self.win, width=20, font=('Times New Roman', 16))
-        self.nameTb.place(x=170, y=240)
+        login_button =Button (
+            self.win,
+            text ="Sign in",
+            font=("Arial", 14),
+            fg="white",
+            bg="#0eafff",
+            activebackground="#0c99dd",
+            activeforeground="white",
+            border=0,
+            width=18,
+            relief="flat",
+            padx=20,
+            pady=5,
+            command=self.sign_in_clicked,
+        )
+        login_button.place(x=right_x + 30, y=entry_y + 140)
+        self.name_entry.bind("<FocusIn>", self.on_entry_click)
+        self.name_entry.bind("<FocusOut>", self.on_focus_out)
 
-        # Vẽ văn bản "Enter your name" trên Canvas
-        custom_font = font.Font(family='Times New Roman', size=18)
-        text_x = 230
-        text_y = 140
-        label.create_text(text_x, text_y, text='Enter your name', font=custom_font, fill='white')
-
-        # Khởi tạo nút Start
-        startBtn = Button(self.win, text='Start', fg='#233657', width=15, command=self.submit_username)
-        startBtn.place(x=225, y=380)
-
-        # Chèn ảnh
-        # Load hình ảnh từ đường dẫn
-        image_path = "asset/static/login_gui/logo.jpg"
-        original_image = Image.open(image_path)
-
-        # Điều chỉnh kích thước ảnh
-        new_size = (250, 250)
-        resized_image = original_image.resize(new_size)
-
-        # Tạo một đối tượng PhotoImage từ hình ảnh
-        photo = ImageTk.PhotoImage(resized_image)
-
-        # Tạo một nhãn (label) để hiển thị hình ảnh và điều chỉnh vị trí
-        label = Label(self.win, image=photo)
-        label.image = photo  # Giữ một tham chiếu đến hình ảnh để tránh bị hủy
-        label.place(x=550, y=135)  # Điều chỉnh vị trí của nhãn
-
-        # hinh 18+
-        image_path = "asset/static/login_gui/18+.png"
-        original_image = Image.open(image_path)
-        new_size = (40, 40)
-        resized_image = original_image.resize(new_size)
-        photo = ImageTk.PhotoImage(resized_image)
-        label = Canvas(self.win, width=50, height=50, bg='#233657', highlightthickness=0)
-        label.create_image(0, 0, anchor='nw', image=photo)
-        label.image = photo
-        label.place(x=50, y=50)
-
-        # logo fb
-        image_path = "asset/static/login_gui/fb.png"
-        original_image = Image.open(image_path)
-        new_size = (30, 30)
-        resized_image = original_image.resize(new_size)
-        photo = ImageTk.PhotoImage(resized_image)
-        label = Canvas(self.win, width=30, height=30, bg='#233657', highlightthickness=0)
-        label.create_image(0, 0, anchor='nw', image=photo)
-        label.image = photo
-        label.place(x=(win_width - fanpageTxt.winfo_reqwidth()) // 2 - 35, y=18)
-
-        # logo twitter
-        image_path = "asset/static/login_gui/twitter.png"
-        original_image = Image.open(image_path)
-        new_size = (30, 30)
-        resized_image = original_image.resize(new_size)
-        photo = ImageTk.PhotoImage(resized_image)
-        label = Canvas(self.win, width=30, height=30, bg='#233657', highlightthickness=0)
-        label.create_image(0, 0, anchor='nw', image=photo)
-        label.image = photo
-        label.place(x=(win_width - fanpageTxt.winfo_reqwidth()) // 2 + 200 - 35, y=18)
-
-        # logo Home
-        image_path = "asset/static/login_gui/home.png"
-        original_image = Image.open(image_path)
-        new_size = (30, 30)
-        resized_image = original_image.resize(new_size)
-        photo = ImageTk.PhotoImage(resized_image)
-        label = Canvas(self.win, width=30, height=30, bg='#233657', highlightthickness=0)
-        label.create_image(0, 0, anchor='nw', image=photo)
-        label.image = photo
-        label.place(x=(win_width - fanpageTxt.winfo_reqwidth()) // 2 - 200 - 35, y=15)
-
-        # Hiển thị cửa sổ
+        # ... Các phần còn lại (ảnh icon, button...) giữ nguyên, nhớ đổi từ win → self.win nếu cần
         self.win.mainloop()
 
-    def change_cursor(self, event):
-        # Thay đổi hình ảnh con trỏ chuột thành hình bàn tay
-        self.win.config(cursor="hand2")
+        # Hàm khi click vào Entry (FocusIn)
+        
+        
+    def on_entry_click(self, event):
+        if self.name_entry.get() == "Nhập tên người chơi":
+            self.name_entry.delete(0, "end")
+            self.name_entry.config(fg="white")
 
-    def revert_cursor(self, event):
-        # Khôi phục hình ảnh con trỏ chuột ban đầu
-        self.win.config(cursor="")
-
-    def open_home(self):
-        webbrowser.open_new("https://fb.com/thegirls.fanpage")
-
-    def open_fanpage(self):
-        webbrowser.open_new("https://google.com")
-
-    def open_twitter(self):
-        webbrowser.open_new("https://youtube.com")
-
-    def submit_username(self):
-        username = self.nameTb.get()
-
+    def on_focus_out(self, event):
+        if self.name_entry.get() == "":
+            self.name_entry.insert(0, "Nhập tên người chơi")
+            self.name_entry.config(fg="#d7d7da")
+    
+    def sign_in_clicked(self):
         # Kiểm tra nếu người dùng đã nhập tên người dùng
+        username = self.name_entry.get().strip()
         if username.strip():
             # Gọi hàm callback và truyền tên người dùng và IP room (có thể là chuỗi rỗng)
-            self.callback[0](username)
             self.win.destroy()  # Đóng cửa sổ sau khi nhấn nút "Submit"
+            self.callback[0](username)
         else:
             # Hiển thị cảnh báo nếu tên người dùng không được nhập
             messagebox.showwarning("Warning", "Vui lòng nhập tên người chơi.")
-    # def setter_usname(self, username):
-    #     self.callback[0](username)
-
-
-def create_client(data):
-    username = data
-    print(f"Creating client for user: {username}")
-
-
-def open_login_window(callback):
-    LoginForm(callback)
-
